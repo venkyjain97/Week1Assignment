eecc916e43c66c8e47f86b8683280bb350b1cd01
GUNJI VENKATESH <93002292+venkyjain97@users.noreply.github.com>
2021-11-15 21:32:22 +0530

    Delete (Day-2)AnyAscii.java

--- (Day-2)AnyAscii.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Ascii{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-     char c = sc.next().charAt(0);    
-System.out.println((int)c);
-    }
-}
