40a5007f7d343ac819c09af73e6f95a34a81cbf1
GUNJI VENKATESH <93002292+venkyjain97@users.noreply.github.com>
2021-11-15 21:31:55 +0530

    Add files via upload

---
+++ (Day-3)AddToThreeAscii.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class AsciiAddPlusThree{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+     char c = sc.next().charAt(0);    
+System.out.println((int)c+3);
+    }
+}
